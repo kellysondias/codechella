d72ddeae1072dd8c5c9add83daf654e09e8f206d
Kellyson Dias <kellysondias@outlook.com>
2023-04-03 18:02:36 -0300

    Fixing responsiveness && adding themes

--- src/components/NavigationMenu/index.tsx
+++ src/components/NavigationMenu/index.tsx
@@ -24,10 +24,14 @@ export const NavigationMenu: React.FC = () => {
 					isActive ? "flex" : "hidden"
 				} absolute font-text text-white before:block before:absolute before:-inset-1 before:bg-summer-blue-100 before:opacity-10 inline-block w-full h-full inset-0 justify-center before:dark:bg-boreal-blue-100 sm:block sm:static sm:bg-transparent sm:before:hidden`}
 			>
-				{/*-top-24*/}
-				<ul className="flex flex-col absolute bg-black text-center top-40 space-x-0 justify-center items-center animate-backInDown sm:flex-row mb-5 opacity-100 md:mb-0 md:space-x-4 lg:space-x-10">
+				<ul className="flex flex-col absolute bg-summer-blue-100 dark:bg-boreal-blue-100 text-center top-40 space-x-0 justify-center items-center p-5 opacity-100  animate-backInDown sm:animate-none sm:static sm:bg-transparent sm:flex-row sm:space-x-10 md:mb-0 md:space-x-4 lg:space-x-10">
 					{navStrings.map((text, index) => (
-						<li key={index}>{text}</li>
+						<li
+							key={index}
+							className="w-full border-b-2 border-white"
+						>
+							{text}
+						</li>
 					))}
 				</ul>
 			</nav>
