08b55651319241f1079390024a19dafa5f55c9f8
Kellyson Dias <kellysondias@outlook.com>
2023-04-17 16:58:23 -0300

    Refactoring

--- src/components/NavigationMenu/index.tsx
+++ src/components/NavigationMenu/index.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
+
 import Strings from "../../strings";
+
+import { List } from "../List";
+import { StyledNav } from "../StyledNav";
 import { ListItem } from "../ListItem";
 import { MobileMenu } from "../MobileMenu";
 
@@ -30,20 +34,11 @@ export const NavigationMenu: React.FC = () => {
 				isActive={isActive}
 				onClick={handleMenu}
 			/>
-			<nav
-				className={`${
-					isActive
-						? "flex"
-						: `animate-backOut transition-opacity ${
-								isHidden && "hidden"
-						} sm:animate-none`
-				} absolute font-text text-white before:block before:absolute before:-inset-1 before:bg-summer-blue-100 before:opacity-10 inline-block w-full h-full inset-0 justify-center items-center before:dark:bg-boreal-blue-100 sm:block sm:static sm:bg-transparent sm:before:hidden`}
+			<StyledNav
+				isActive={isActive}
+				isHidden={isHidden}
 			>
-				<ul
-					className={`flex flex-col absolute ${
-						isActive ? "animate-backInDown" : "animate-backOutUp"
-					} bg-summer-blue-100 dark:bg-boreal-blue-100 text-center top-40 space-x-0 justify-center items-center p-5 opacity-100  sm:animate-none sm:static sm:bg-transparent sm:flex-row sm:space-x-10 md:mb-0 md:space-x-4 lg:space-x-10`}
-				>
+				<List isActive={isActive}>
 					{navStrings.map((text, index) => (
 						<ListItem
 							index={index}
@@ -53,8 +48,8 @@ export const NavigationMenu: React.FC = () => {
 							{text}
 						</ListItem>
 					))}
-				</ul>
-			</nav>
+				</List>
+			</StyledNav>
 		</div>
 	);
 };
---
+++ src/components/StyledNav/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface StyledNavProps {
+	isActive: boolean;
+	isHidden: boolean;
+	children: React.ReactNode;
+}
+
+export const StyledNav: React.FC<StyledNavProps> = ({
+	isActive,
+	isHidden,
+	children,
+}) => (
+	<nav
+		className={`${
+			isActive
+				? "flex"
+				: `animate-backOut ${
+						isHidden && "hidden"
+				} sm:animate-none`
+		} absolute font-text text-white before:block before:absolute before:-inset-1 before:bg-summer-blue-100 before:opacity-10 inline-block w-full h-full inset-0 justify-center items-center before:dark:bg-boreal-blue-100 sm:block sm:static sm:bg-transparent sm:before:hidden`}
+	>
+		{children}
+	</nav>
+);
