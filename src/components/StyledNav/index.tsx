e00d849adb6eb7928b7912950c0561fd86ae7868
Kellyson Dias <kellysondias@outlook.com>
2023-04-18 14:30:59 -0300

    Fixing the mobile menu's animation

--- src/components/StyledNav/index.tsx
+++ src/components/StyledNav/index.tsx
@@ -15,7 +15,7 @@ export const StyledNav: React.FC<StyledNavProps> = ({
 		className={`${
 			isActive
 				? "flex"
-				: `animate-backOut ${
+				: `flex animate-backOut ${
 						isHidden && "hidden"
 				} `
 		} absolute font-text text-white before:block before:absolute before:-inset-1 before:bg-summer-blue-100 before:opacity-10 inline-block w-full h-full inset-0 justify-center items-center before:dark:bg-boreal-blue-100 sm:block sm:static sm:bg-transparent sm:before:hidden sm:animate-none`}
