be53b5d258b4528009931de27abad41124ec387f
Kellyson Dias <kellysondias@outlook.com>
2023-04-19 15:24:38 -0300

    Update StyledNav

--- src/pages/Header/StyledNav/index.tsx
+++ src/pages/Header/StyledNav/index.tsx
@@ -13,12 +13,8 @@ export const StyledNav: React.FC<StyledNavProps> = ({
 }) => (
 	<nav
 		className={`${
-			isActive
-				? "flex"
-				: `flex animate-backOut ${
-						isHidden && "hidden"
-				} `
-		} absolute text-white before:block before:absolute before:-inset-1 before:bg-summer-blue-100 before:opacity-10 inline-block w-full h-full inset-0 justify-center items-center before:dark:bg-boreal-blue-100 sm:block sm:static sm:bg-transparent sm:before:hidden sm:animate-none`}
+			isActive ? "flex" : `flex animate-backOut ${isHidden && "hidden"} `
+		} absolute text-white bg-summer-blue-100/10 inline-block w-full h-full inset-0 justify-center items-center dark:bg-boreal-blue-100/10 sm:block sm:static sm:bg-transparent sm:before:hidden sm:animate-none`}
 	>
 		{children}
 	</nav>
