f97e7e2a9ad9cce437816568699d10178047b3c7
Kellyson Dias <kellysondias@outlook.com>
2023-04-19 16:23:33 -0300

    Finishing the Hero component

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -4,7 +4,7 @@ import { NavigationMenu } from "./NavigationMenu";
 import { ThemeToggleButton } from "./ThemeToggleButton";
 
 const Header: React.FC = () => (
-	<header className="bg-summer-blue-100 flex flex-row justify-evenly items-center text-center dark:bg-boreal-blue-100 sm:flex-col md:flex-row z-50">
+	<header className="bg-summer-blue-100 flex flex-row justify-evenly items-center text-center dark:bg-boreal-blue-100 sm:flex-col md:px-3 md:flex-row z-50 lg:px-0">
 		<HeaderLogo />
 		<NavigationMenu />
 		<ThemeToggleButton />
--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -6,9 +6,11 @@ export const Hero: React.FC = () => {
 	const heroString = Strings.pages.home.hero;
 
 	return (
-		<div className="flex justify-center flex-col bg-fixed bg-summer-banner w-full h-[270px] bg-no-repeat bg-cover text-summer-hero text-center text-3xl dark:bg-boreal-banner dark:text-boreal-hero">
-			<p>{heroString.lineOne}</p>
-            <p>{heroString.lineTwo}</p>
+		<div className="bg-summer-banner w-full h-96 bg-no-repeat bg-cover bg-center dark:bg-boreal-banner">
+			<div className="flex justify-center flex-col text-center text-3xl text-summer-hero w-full h-full bg-white/25 dark:bg-transparent dark:text-boreal-hero sm:text-6xl">
+                <p>{heroString.lineOne}</p>
+                <p>{heroString.lineTwo}</p>
+            </div>
 		</div>
 	);
 };
