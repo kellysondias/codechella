1bd3064bb5010920b81fda3473556767c01cd5d8
Kellyson Dias <kellysondias@outlook.com>
2023-07-04 17:23:04 -0300

    Update

--- src/components/Header/NavigationMenu/ScrollHandler/index.ts
+++ src/components/Header/NavigationMenu/ScrollHandler/index.ts
@@ -1,22 +1,14 @@
-import isDesktop from "../../../../services/functions/isDesktop";
-
 export default class ScrollHandler {
-  private hideOverflow = document.body.classList.add("overflow-y-hidden");
-  private showOverflow = document.body.classList.remove("overflow-y-hidden");
-
   #setPosition = (position: (event: Event) => void) => {
     window.onscroll = position;
   };
 
   disable = () => {
     const scrollTop = document.documentElement.scrollTop;
-    if (isDesktop) this.#setPosition(() => window.scrollTo(0, scrollTop));
-    else this.hideOverflow;
-    // return this.#setPosition(() => window.scrollTo(0, scrollTop));
+    return this.#setPosition(() => window.scrollTo(0, scrollTop));
   };
 
   enable = () => {
-    if (isDesktop) this.#setPosition(() => null);
-    else this.showOverflow;
+    return this.#setPosition(() => null);
   };
 }
