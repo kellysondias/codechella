7572591faf3dd1468b89d3b63c5a2c509ad3f5b0
Kellyson Dias <kellysondias@outlook.com>
2023-07-04 16:27:15 -0300

    Update "RowItem"

--- src/pages/Home/LineUp/LineUpSection/LineUpList/RowItem/index.tsx
+++ src/pages/Home/LineUp/LineUpSection/LineUpList/RowItem/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 
 export const RowItem: React.FC<{ row: string[] }> = ({ row }) => {
-	return (
-		<>
-			{row.map((item, index) => (
-				<li
-					key={index}
-					className="text-center mb-4 mx-5"
-				>
-					{item}
-				</li>
-			))}
-		</>
-	);
+  return (
+    <>
+      {row.map((item, index) => (
+        <li key={index} className="text-center mb-4 mx-5">
+          {item}
+        </li>
+      ))}
+    </>
+  );
 };
